69c8ae5c040a1c98110bfc0d059ed96907832a53
rykyou <rkim1214@gmail.com>
2019-06-30 18:44:09 -0400

    Create new artwork show page route

--- app/controllers/api/v1/artworks_controller.rb
+++ app/controllers/api/v1/artworks_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::ArtworksController < ApplicationController
 
   def initial
-    url = 'https://api.thewalters.org/v1/collections/7/objects.json?apikey='+api_key
+    url = 'https://api.thewalters.org/v1/objects?keyword=landscape&classification=painting&medium=oil&pageSize=100&apikey='+api_key
+    response = RestClient.get(url).body
+    render json: response
+  end 
+
+  def show
+    artwork_id = params[:id]
+    url = 'https://api.thewalters.org/v1/objects/'+artwork_id+'.json?apikey='+api_key
     response = RestClient.get(url).body
     render json: response
   end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :artworks, only: [:show]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
 
       get '/artworks/initial', to: 'artworks#initial'
+      # get '/artworks/:id', to: 'artworks#artworks_detail'
     end
   end
 end

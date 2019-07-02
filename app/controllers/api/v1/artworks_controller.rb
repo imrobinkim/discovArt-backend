fe7c5eee0b7244fd04cd4d7d3ea79695197e7c2f
rykyou <rkim1214@gmail.com>
2019-07-01 20:43:24 -0400

    Add search by keyword route

--- app/controllers/api/v1/artworks_controller.rb
+++ app/controllers/api/v1/artworks_controller.rb
@@ -1,12 +1,27 @@
 class Api::V1::ArtworksController < ApplicationController
 
   def initial
-    url = 'https://api.thewalters.org/v1/objects?keyword=landscape&classification=painting&medium=oil&pageSize=100&apikey='+api_key
+    url = 'https://api.thewalters.org/v1/objects.json?keyword=landscape&classification=painting&medium=oil&pageSize=100&apikey='+api_key
     response = RestClient.get(url).body
     render json: response
   end 
 
-  def show
+  def search_by_keyword
+    keyword = params['keyword']
+    if keyword.include?(' ')
+      keyword_arr = keyword.gsub(/\s+/m, ' ').strip.split(" ")
+      keyword = keyword_arr[0]
+      keyword_arr.shift()
+      for x in keyword_arr do
+        keyword = keyword + '%20' + x
+      end
+    end
+    url = 'https://api.thewalters.org/v1/objects.json?keyword='+keyword+'&apikey='+api_key
+    response = RestClient.get(url).body
+    render json: response
+  end
+
+  def artwork_detail
     artwork_id = params[:id]
     url = 'https://api.thewalters.org/v1/objects/'+artwork_id+'.json?apikey='+api_key
     response = RestClient.get(url).body
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :artworks, only: [:show]
+      # resources :artworks, only: [:show]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
 
       get '/artworks/initial', to: 'artworks#initial'
-      # get '/artworks/:id', to: 'artworks#artworks_detail'
+      get '/artworks/bykeyword', to: 'artworks#search_by_keyword'
+      get '/artworks/:id', to: 'artworks#artwork_detail'
     end
   end
 end
